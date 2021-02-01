bd5f59024ba8b92395126d1e3dea15cbe2dc461b
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-02-01 07:08:52 -0500

    Initial

---
+++ lib/nerves_environment_sensor.ex
@@ -0,0 +1,18 @@
+defmodule NervesEnvironmentSensor do
+  @moduledoc """
+  Documentation for NervesEnvironmentSensor.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesEnvironmentSensor.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_environment_sensor_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesEnvironmentSensorTest do
+  use ExUnit.Case
+  doctest NervesEnvironmentSensor
+
+  test "greets the world" do
+    assert NervesEnvironmentSensor.hello() == :world
+  end
+end
