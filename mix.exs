1a5f87618b8f4091efe23cef21e86fc41f38287c
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-04-30 17:15:09 -0400

    Add :inets to the :extra_applications list so that I can use the weather command

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule HelloNerves.MixProject do
   def application do
     [
       mod: {HelloNerves.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :inets]
     ]
   end
 
