773dc0fdb70a0b8bfb5f90f185036c1ba6567cd0
Masatoshi N <nishiguchi.masa@gmail.com>
2022-01-14 10:39:15 -0500

    Check Code.ensure_loaded?(HelloNerves) in .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,10 +1,10 @@
 import_file_if_available("~/.iex.exs")
 
-alias HelloNerves.Worker
+if Code.ensure_loaded?(HelloNerves) do
+  import_if_available(HelloNerves)
+  import_if_available(Mox)
 
-import HelloNerves
-import Mox
-
-Mox.set_mox_from_context([])
-Mox.stub_with(HelloNerves.MockSensorDevice, HelloNerves.SensorDevice.Stub)
-Mox.stub_with(HelloNerves.MockSensorApi, HelloNerves.SensorApi.Stub)
+  Mox.set_mox_from_context([])
+  Mox.stub_with(HelloNerves.MockSensorDevice, HelloNerves.SensorDevice.Stub)
+  Mox.stub_with(HelloNerves.MockSensorApi, HelloNerves.SensorApi.Stub)
+end
